36b3a440c30fb1b8e65f338b96c52092a89a348f
shamshath-begum <sham29.b@gmail.com>
2023-02-06 15:38:08 +0530

    hi

---
+++ calc.js
@@ -0,0 +1,72 @@
+console.log("hello")
+console.log("hello shama")
+console.log("hello shama")
+
+
+// let fs= require("fs")
+// fs.writeFile("sample.txt","i am fine","utf-8",(err)=>{
+//     if(err)
+//     console.log(err);
+// })
+// fs.readFile('sample.txt','utf-8' ,(err,data)=>{
+// console.log(data);
+// })
+// fs.unlink("sample1.txt",(err)=>{
+    
+//     console.log("deleted");
+// })
+
+
+// const fs=require('fs');
+// fs.readFile('sample.txt','utf-8',(err,data)=>{
+//     console.log(data);
+// })
+// fileName="sample.txt";
+// let server=http.createServer((req,res)=>{
+//     res.writeHead(200,{'content-Type':'text/html'});
+//     res.end(`<div>
+//     <h1>hello</h1>
+// </div>`
+//     )
+// })
+// server.listern(8000,()=>{
+//     console.log("Server is liening");
+// })
+// console.log("hello bilal");
+// let c=2+4;
+// console.log(c);
+
+// let http=require('http')
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html'})
+//     res.write("hello shama h r u")
+//     res.end()
+// }).listen(8000)
+
+// module
+// let calc=require('./calc.js')
+// let result=calc.add(2,3)
+// console.log(result);
+
+
+// read
+
+// let fs=require('fs')
+// fs.readFile('calc.js','utf8',function(err,data
+//     console.log(data);
+// })
+
+// write
+
+// let fs=require('fs')
+// fs.appendFile('calc.js','console.log("hello shama h r u")',function(err){
+//     console.log("done")
+// })
+// delete
+
+// let fs=require('fs')
+// fs.unlink('calc.js',function(err){
+//     console.log("deleted");
+// })
+// let os=require('os')
+// console.log(os.freemem()/12);
